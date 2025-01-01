9e3af9c2ef94eba0f8fa845c4421e1d8441df47d
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-01-02 07:57:48 +0900

    taskList, Focus 구현

--- src/manager/windowManager.jsx
+++ src/manager/windowManager.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
+import Application from '../applications/application.jsx';
+import Discover from "../applications/discover.jsx";
 
 const WindowManager = () => {
   const displayDriver = {
     position: "fixed",
     inset: 0
-  }
+  };
+  const [layer, setLayer] = useState(1);
+  const [focus,setFocus] = useState(null);
+  const [taskList, setTaskList] = useState([]);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setTaskList(Temp=> [...Temp,
+        {
+          "component":<Discover taskList={taskList} setTaskList={setTaskList}/>,
+          "type":"Shell",
+          "id":taskList.length,
+          "layer":taskList.length
+        }
+      ])
+    }, 200)
+  },[])
   return(
     <main className="window-manager" style={displayDriver}>
-
+      {
+        taskList.map((task) => {
+          return(
+            <Application key={task.id}
+                         uid={task.id}
+                         type={task.type}
+                         layer={layer}
+                         setLayer={setLayer}
+            >{task.component}</Application>
+          )
+        })
+      }
     </main>
   )
 }
