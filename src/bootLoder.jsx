f266cfd3fccfd3b2a0039669a695235059bad2ee
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-01-02 07:54:32 +0900

    기본 테마 추가

--- src/bootLoder.jsx
+++ src/bootLoder.jsx
@@ -1,9 +1,6 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Kernel from './services/kernel.jsx'
+import './Theme.css'
 createRoot(document.getElementById('display')).render(
-  <StrictMode>
-    <Kernel />
-  </StrictMode>,
+  <Kernel />
 )
