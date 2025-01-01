0f79bab705034b16ea5adbd3d300e71ae7ac2122
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-01-02 07:56:31 +0900

    kernel 파일 수정

--- src/services/kernel.jsx
+++ src/services/kernel.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import WindowManiger from '../manager/windowManager.jsx';
-import ProcessManager from '../manager/processManager.js';
 
 function Kernel() {
-  const processManager = new ProcessManager();
   return (
     <div className="kernel">
-      <WindowManiger PM={processManager} />
+      <WindowManiger />
     </div>
   )
 }
