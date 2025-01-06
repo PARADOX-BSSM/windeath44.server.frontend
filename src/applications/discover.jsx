ec171cef12b3033a174e1f91f18b3702e1143d87
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-01-06 11:04:53 +0900

    중복 제거

--- src/applications/discover.jsx
+++ src/applications/discover.jsx
@@ -1,32 +1,32 @@
 import Application from "./application.jsx";
 
 const Discover = ({taskList, setTaskList}) => {
+  const Application1 = {
+    "component": <></>,
+    "type": "App",
+    "id": 1234,
+    "layer":taskList.length,
+    "name":"Application"
+  }
+  const Application2 = {
+    "component": <>hello</>,
+    "type": "App",
+    "id": 2345,
+    "layer":taskList.length,
+    "name":"Application2"
+  }
   let key = 1;
   return(
     <>
       <>
         <button onClick={() => {
-          setTaskList(Temp => [...Temp,
-            {
-              "component": <></>,
-              "type": "App",
-              "id": key++,
-              "layer":taskList.length,
-              "name":"Application"
-            }
-          ])
+          setTaskList(Temp => (!Temp.includes(Application1))?
+            [...Temp, Application1]:[...Temp])
         }}>Application
         </button>
         <button onClick={() => {
-          setTaskList(Temp => [...Temp,
-            {
-              "component": <>hello</>,
-              "type": "App",
-              "id": key++,
-              "layer":taskList.length,
-              "name":"Application2"
-            }
-          ])
+          setTaskList(Temp => (!Temp.includes(Application2))?
+            [...Temp, Application2]:[...(Temp.splice(1,1,Application1))])
         }}>Application2
         </button>
       </>
