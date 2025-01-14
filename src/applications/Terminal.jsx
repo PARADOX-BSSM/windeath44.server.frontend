9c0fdde4a02ca8bd68a18fbeb41fcc44b87c2670
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-01-14 13:13:34 +0900

    Terminal 생성

---
+++ src/applications/Terminal.jsx
@@ -0,0 +1,13 @@
+const Terminal = () =>{
+  const styles = {
+    backgroundColor: "black",
+    height: "100%",
+    width: "100%"
+  }
+  return (
+    <div style={styles}>
+
+    </div>
+  )
+}
+export default Terminal
