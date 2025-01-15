0ab79823aca4268435ff35eb4627a0e96c093769
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-01-15 15:17:44 +0900

    Terminal Input창 생성

--- src/applications/Terminal.jsx
+++ src/applications/Terminal.jsx
@@ -1,12 +1,22 @@
+import {useState} from "react";
 const Terminal = () =>{
-  const styles = {
+  const terminalStyles = {
     backgroundColor: "black",
     height: "100%",
     width: "100%"
   }
-  return (
-    <div style={styles}>
+  const inputStyles = {
+    backgroundColor: "none",
+    width : "100%",
 
+  }
+  const [command,setCommand] = useState("");
+  return (
+    <div style={terminalStyles}>
+      <input style={inputStyles} onKeyDown={(e)=>{
+        if(e.key==="Enter"){
+          console.log("enter")
+        }}}></input>
     </div>
   )
 }
