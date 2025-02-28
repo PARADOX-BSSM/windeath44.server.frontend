2fe46a0a2b2c812b7f096291868bf985295db5ff
yuuki08noah <yuuki08noah@gmail.com>
2025-02-28 20:31:58 +0900

    feat :: Dockerfile, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: {
+      origin: 'http://yuuki08noah.com', // 허용할 도메인
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
+      allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
+      credentials: true, // 쿠키를 포함할지 여부
+    },
+    port: 3000,
+    host: '0.0.0.0'
+   },
 })
