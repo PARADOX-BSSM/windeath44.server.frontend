522eff94e074ed1d06365435ab76563b36789e42
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-03-07 14:08:28 +0900

    refactor :: 코드 분해

---
+++ src/applications/settings.jsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import {useStack} from '../manager/dataStructureManager.jsx'
+
+import {Main, Wallpaper} from './Pages/settingsPages.jsx'
+
+const Settings = () => {
+  const [stack, Push, Pop, Top] = useStack();
+  const [signal, setSignal] = useState(null);
+  useEffect(() => {
+    console.log(stack);
+  }, [stack]);
+  useEffect(() => {
+    if(signal!==null) {
+      if(signal==="back") {
+        Pop()
+      }else {
+        Push(signal);
+        setSignal(null);
+      }
+    }
+  },  [signal])
+    return (
+      <>
+        <Main number={Top()} setSignal={setSignal}/>
+        <Wallpaper number={Top()} setSignal={setSignal}/>
+      </>
+    )
+
+}
+export default Settings;
