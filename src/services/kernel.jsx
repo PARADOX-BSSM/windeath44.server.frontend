39ac87ec1a619968818661e6acc203f831fd0157
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-03-07 14:07:43 +0900

    refactor :: 오타 수정

--- src/services/kernel.jsx
+++ src/services/kernel.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import WindowManiger from '../manager/windowManager.jsx';
+import WindowManager from '../manager/windowManager.jsx';
 
 function Kernel() {
   //작업 관리를 위한 메니저 호출
   return (
     <div className="kernel">
-      <WindowManiger />
+      <WindowManager />
     </div>
   )
 }
