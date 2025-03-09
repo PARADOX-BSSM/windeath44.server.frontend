fd47498c419a209bcde7a970c46275b22a224ba8
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-03-09 23:42:39 +0900

    refactor :: typescript로 마이그레이션 초기 세팅

--- src/bootLoder.tsx
+++ src/bootLoder.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import Kernel from './services/kernel.jsx'
+import Kernel from './services/kernel.tsx'
 import './Theme.css'
-createRoot(document.getElementById('display')).render(
+createRoot(document.getElementById('display') as HTMLElement).render(
   //커널 스크립트 호출
   <Kernel />
 )
