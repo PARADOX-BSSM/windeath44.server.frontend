e930ada70c368835af787ec01a00f2d6ce284b1f
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2025-03-12 09:10:53 +0900

    feat :: RecoilRoot 생성

--- src/bootLoder.tsx
+++ src/bootLoder.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import Kernel from './services/kernel.tsx'
 import './Theme.css'
+import {RecoilRoot} from 'recoil'
 createRoot(document.getElementById('display') as HTMLElement).render(
   //커널 스크립트 호출
-  <Kernel />
+  <RecoilRoot>
+    <Kernel />
+  </RecoilRoot>
 )
