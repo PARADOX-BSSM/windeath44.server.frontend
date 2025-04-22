062520b56945d2453e65ee9d25a612b3823c69c1
soeun823 <bsoeun866@gmail.com>
2025-04-22 09:55:59 +0900

    feat :: input 컴포넌트 분리

---
+++ src/applications/utility/findId/index.tsx
@@ -0,0 +1,8 @@
+import * as _ from './style';
+
+const FindId = () => {
+    return(
+        <></>
+    );
+}
+export default FindId;
---
+++ src/applications/utility/findId/style.ts
@@ -0,0 +1,2 @@
+import styled from 'styled-components';
+import '@/assets/font.css';
