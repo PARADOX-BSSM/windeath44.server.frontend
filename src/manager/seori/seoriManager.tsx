4e11e75c0a8850245c40b81d27bef36e70dc6b75
Roena0516 <tkf2001.yc@gmail.com>
2025-04-24 17:45:29 +0900

    feat :: 벽 마찰력 제거

--- src/manager/seori/seoriManager.tsx
+++ src/manager/seori/seoriManager.tsx
@@ -9,6 +9,8 @@ export default function Seori() {
   const isDraggingRef = useRef(false);
   // 설이가 바라보는 방향 Ref
   const directionRef = useRef("left");
+  // 설이 상태 Ref
+  const stateRef = useRef("default");
   
   useEffect(() => {
     const container = document.getElementById("cursorContainer");
@@ -44,21 +46,25 @@ export default function Seori() {
     // 벽
     const leftWall = Bodies.rectangle(-50, 0, 100, bounds.height * 2, {
       isStatic: true, // isStatic: false로 되어있으면 얘도 떨어짐
+      friction: 0,
       render: { fillStyle: "#E6B143"},
       label: "left"
     })
     const rightWall = Bodies.rectangle(bounds.width + 50, 0, 100, bounds.height * 2, {
       isStatic: true,
+      friction: 0,
       render: { fillStyle: "#E6B143"},
       label: "right"
     })
     const ground = Bodies.rectangle(0, bounds.bottom - taskbarBounds.height / 2, taskbarBounds.width * 10, taskbarBounds.height, {
       isStatic: true,
+      friction: 1,
       render: { fillStyle: "#E6B143"},
       label: "ground"
     })
     const top = Bodies.rectangle(310, -50, bounds.width * 2, 100, {
       isStatic: true,
+      friction: 0,
       render: { fillStyle: "#E6B143"},
       label: "top"
     })
@@ -69,20 +75,45 @@ export default function Seori() {
     const runner = Runner.create()
     Runner.run(runner, engine)
 
-    // 설이
+    function loadImageSize(src: string): Promise<{ width: number; height: number }> {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.onload = () => resolve({ width: img.width, height: img.height });
+          img.onerror = reject;
+          img.src = src;
+        });
+    }
+
     let shape = Bodies.rectangle(300, 150, 100, 150, {
-      inertia: Infinity,
-      render: {
-        sprite: { 
-          xScale: 100,
-          yScale: 100,
-          texture: ""
+        inertia: Infinity,
+        render: {
+        sprite: {
+            texture:  `src/assets/seori_${stateRef.current}.png`,
+            xScale: 1,
+            yScale: 1,
         }
-      },
-      label: "shape"
+        },
+        label: "shape"
     });
-    World.add(world,shape);
+
+    // 설이
+    const texturePath = `src/assets/seori_${stateRef.current}.png`;
+    loadImageSize(texturePath).then(({ width, height }) => {
+        shape = Bodies.rectangle(300, 150, width, height, {
+            inertia: Infinity,
+            render: {
+            sprite: {
+                texture: texturePath,
+                xScale: 1,
+                yScale: 1,
+            }
+            },
+            label: "shape"
+        });
+
+    World.add(world, shape);
     shapeRef.current = shape;
+    });
 
     // 드래그 시작 && 끝나면 실행되는 함수들
     const onDragStart = () => {
