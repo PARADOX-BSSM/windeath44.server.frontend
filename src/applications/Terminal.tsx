4ab7aea679aa1497357d1186df041210a12c1e99
winshine0326 <shane10340@gmail.com>
2025-05-14 19:26:07 +0900

    docs :: dataStructureModule 위치 modules -> hooks로 변경

--- src/applications/Terminal.tsx
+++ src/applications/Terminal.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {styled} from "styled-components";
-import {useQueue} from "@/modules/dataStructureModule.tsx";
+import {useQueue} from "@/hooks/dataStructureModule";
 
 const Input = styled.input`
     background-color: none;
