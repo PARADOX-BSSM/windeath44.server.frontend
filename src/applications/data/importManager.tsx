9bbdd04c8eda28ab763f25c8b1cb6f81f7a15a48
winshine0326 <shane10340@gmail.com>
2025-05-14 20:21:44 +0900

    docs :: terminal -> applicationList로 위치 변경

--- src/applications/data/importManager.tsx
+++ src/applications/data/importManager.tsx
@@ -1,7 +1,7 @@
 import {Suspense, lazy} from "react";
 import {TaskType} from "@/modules/typeModule.tsx";
 
-const Terminal =  lazy(()=> import("../Terminal.tsx"));
+const Terminal =  lazy(()=> import("../applicationList/Terminal.tsx"));
 const Settings = lazy(()=> import("../settings.tsx"));
 const LogIn = lazy(()=> import("../utility/login/index.tsx"));
 
