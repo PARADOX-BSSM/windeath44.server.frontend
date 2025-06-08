6fd927a1dce4c1d0a72af7cd07aca7adcb24f11a
winshine0326 <shane10340@gmail.com>
2025-06-08 21:58:45 +0900

    feat :: 임시로 processManager recoil -> jotai로 변경

--- src/hooks/processManager/processManager.tsx
+++ src/hooks/processManager/processManager.tsx
@@ -1,22 +1,16 @@
-import {TaskType} from '../../modules/typeModule.tsx'
-import {useRecoilState} from "recoil";
-import {taskManager} from "@/hooks/processManager/state.ts";
+import { TaskType } from '../../modules/typeModule.tsx'
+import { useAtom } from 'jotai'
+import { taskManagerAtom } from './state'
 
-//프로세스 관리 훅
-//기본 제공 기능
-//- 프로세스 추가
-//- 프로세스 삭제 
 const useProcessManager: () => [TaskType[], (component: TaskType) => void, (component: TaskType) => void] = () => {
-  const [taskList, setTaskList] = useRecoilState<TaskType[]>(taskManager);
-  const addTask = (component:TaskType) => {
-    setTaskList(Task => (!Task.includes(component))?
-      [...Task, component]:[...Task])
-  };
-  const removeTask = (component:TaskType) => {
-    setTaskList(Task => (Task.some(item => item.name === component.name)) ?
-      Task.filter(item => item.name !== component.name) : [...Task])
-  };
-  return [taskList, addTask, removeTask];
+  const [taskList, setTaskList] = useAtom(taskManagerAtom)
+  const addTask = (component: TaskType) => {
+    setTaskList(Task => (!Task.some(item => item.name === component.name)) ? [...Task, component] : [...Task])
+  }
+  const removeTask = (component: TaskType) => {
+    setTaskList(Task => Task.filter(item => item.name !== component.name))
+  }
+  return [taskList, addTask, removeTask]
 }
 
-export {useProcessManager};
+export { useProcessManager }
--- src/hooks/processManager/state.ts
+++ src/hooks/processManager/state.ts
@@ -1,10 +1,4 @@
-import {atom, RecoilState} from 'recoil'
-import {TaskType} from '@/modules/typeModule.tsx'
+import { atom } from 'jotai'
+import { TaskType } from '@/modules/typeModule.tsx'
 
-const taskManager:RecoilState<TaskType[]> = atom({
-  key: 'taskManager',
-  default: [] as any
-})
-// This atom will be used to manage the state of the task manager
-// It will hold an array of TaskType objects, which represent the tasks in the task manager                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-export {taskManager}
+export const taskManagerAtom = atom<TaskType[]>([])
