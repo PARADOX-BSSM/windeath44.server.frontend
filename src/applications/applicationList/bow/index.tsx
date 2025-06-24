f2dcc6a96a32995e737cef48899ce487b8466221
soeun823 <bsoeun866@gmail.com>
2025-06-24 20:28:23 +0900

    feet :: [FRONTEND-50] 퍼블리싱

--- src/applications/applicationList/bow/index.tsx
+++ src/applications/applicationList/bow/index.tsx
@@ -1,13 +1,19 @@
 import * as _ from "@/applications/applicationList/bow/style.ts";
 import Table from "@/assets/bow/table.svg";
 import Character from "@/assets/character/hosino.svg";
+import {useState} from "react";
 
 const Bow = () => {
+    const [bow,setBow]=useState<number>(0);
+    const countBow= ()=>{
+        setBow((bow+1))
+    };
+
     return(
         <_.main>
             <_.nbow>
                 <div>절하고 간 사람</div>
-                <div>: {1010}명</div>
+                <div>: {bow}명</div>
             </_.nbow>
             <_.place>
                 <_.imgs>
@@ -18,7 +24,7 @@ const Bow = () => {
                         <div>
                             <div>
                                 <div>
-                                    <button>절</button>
+                                    <button onClick={countBow}>절</button>
                                 </div>
                             </div>
                         </div>
