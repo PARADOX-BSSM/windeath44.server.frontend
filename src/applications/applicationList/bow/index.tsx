56cb865cebee7501d04c0bef1ed4e4763674dabb
soeun823 <bsoeun866@gmail.com>
2025-06-24 19:16:18 +0900

    feet :: [FRONTEND-50] 절하기 창 생성

---
+++ src/applications/applicationList/bow/index.tsx
@@ -0,0 +1,15 @@
+import * as _ from "@/applications/applicationList/bow/style.ts";
+
+const Bow = () => {
+    return(
+        <_.main>
+            <div>
+                <div>절하고 간 사람</div>
+                <div>: {1010}명</div>
+            </div>
+            <img alt={"캐릭터"}/>
+            <img alt={"테이블"}/>
+        </_.main>
+    )
+}
+export default Bow;
---
+++ src/applications/applicationList/bow/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+export const main = styled.div`
+    box-sizing: border-box;
+    background-color: var(--light-primary-color);
+    font-family: "Galmuri11";
+    font-size: 16px;
+    width: 100%;
+    height: 100%;
+    padding: 12px 24px;
+`;
