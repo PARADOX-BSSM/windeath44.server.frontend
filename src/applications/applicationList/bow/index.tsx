9a586acea279b3b8f0ea6c263b2caadd3dbc499e
soeun823 <bsoeun866@gmail.com>
2025-06-24 20:20:42 +0900

    feet :: [FRONTEND-50] 스타일 추가

--- src/applications/applicationList/bow/index.tsx
+++ src/applications/applicationList/bow/index.tsx
@@ -1,14 +1,29 @@
 import * as _ from "@/applications/applicationList/bow/style.ts";
+import Table from "@/assets/bow/table.svg";
+import Character from "@/assets/character/hosino.svg";
 
 const Bow = () => {
     return(
         <_.main>
-            <div>
+            <_.nbow>
                 <div>절하고 간 사람</div>
                 <div>: {1010}명</div>
-            </div>
-            <img alt={"캐릭터"}/>
-            <img alt={"테이블"}/>
+            </_.nbow>
+            <_.place>
+                <_.imgs>
+                    <_.character src={Character} alt={"캐릭터"}/>
+                    <_.table src={Table} alt={"테이블"}/>
+                </_.imgs>
+                <_.bbow>
+                        <div>
+                            <div>
+                                <div>
+                                    <button>절</button>
+                                </div>
+                            </div>
+                        </div>
+                </_.bbow>
+            </_.place>
         </_.main>
     )
 }
