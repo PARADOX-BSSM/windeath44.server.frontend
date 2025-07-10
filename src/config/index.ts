3419786e09f5b97084dec845a85043cf1c2d25a6
soeun823 <bsoeun866@gmail.com>
2025-07-10 14:29:29 +0900

    docs :: [FRONTEND-56] 변수명 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
-const port ='http://';
+const protocol ='http://';
 const server ='10.129.57.45';
-export const auth = `${port}${server}:4444/auth`;//회원가압
-export const user = `${port}${server}:4445/users`;//사용자 정보
-export const memorial = `${port}${server}:4446/memorial`;//추모관
-export const memorial_application = `${port}${server}:4447/memorial_application`;//추모관 신청
-export const anime = `${port}${server}:4448/anime`;//에니메이션
+export const auth = `${protocol}${server}:4444/auth`;//회원가압
+export const user = `${protocol}${server}:4445/users`;//사용자 정보
+export const memorial = `${protocol}${server}:4446/memorial`;//추모관
+export const memorial_application = `${protocol}${server}:4447/memorial_application`;//추모관 신청
+export const anime = `${protocol}${server}:4448/anime`;//에니메이션
