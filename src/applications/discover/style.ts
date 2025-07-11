b45f93e5fad8aef8acd1cbd7ed316a4bc0e31018
Roena0516 <tkf2001.yc@gmail.com>
2025-07-11 11:55:40 +0900

    fix :: icon 스타일 변경

--- src/applications/discover/style.ts
+++ src/applications/discover/style.ts
@@ -1,14 +1,15 @@
+import { getPixelFromPercent } from "@/lib/getPixelFromPercent";
 import styled from "@emotion/styled";
 
 export const AppContainer = styled.div`
     width:100%;
     height:100%;
-    margin-top: 3rem;
-    margin-left: 2rem;
+    margin-top: ${getPixelFromPercent('height', 2)}px;
+    margin-left: ${getPixelFromPercent('height', 2)}px;
 `
 
 export const AppBtn = styled.div<{ url?: string }>`
-    height:75%;
+    height: ${getPixelFromPercent('height', 8)}px;
     aspect-ratio: 1/1;
     margin: 0 auto;
     background-image: url('${({ url }) => url}');
@@ -17,11 +18,11 @@ export const AppBtn = styled.div<{ url?: string }>`
 `
 
 export const AppName = styled.p`
-    margin: 0.5em auto;
+    margin: ${getPixelFromPercent('height', 1)}px auto;
     color: var(--primary-black);
     text-align: center;
     font-family: Galmuri11;
-    font-size: 0.825rem;
+    font-size: 0.9rem;
     font-style: normal;
     font-weight: 400;
     line-height: 18px;
