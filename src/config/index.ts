363175b3bdf6fbc76ab57aaca7eba7243ac102ca
soeun823 <bsoeun866@gmail.com>
2025-07-14 01:44:41 +0900

    chore :: [FRONTEND-74] 서버 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
-const protocol = 'http://';
+const protocol = 'https://';
 const server = import.meta.env.VITE_SERVER;
-export const auth = `${protocol}${server}:4444/auth`; //회원가압
-export const user = `${protocol}${server}:4445/users`; //사용자 정보
-export const memorial = `${protocol}${server}:4446/memorial`; //추모관
-export const memorial_application = `${protocol}${server}:4447/memorial_application`; //추모관 신청
-export const anime = `${protocol}${server}:4448/anime`; //에니메이션
+export const auth = `${protocol}${server}/auth`; //회원가압
+export const user = `${protocol}${server}/users`; //사용자 정보
+export const memorial = `${protocol}${server}/memorial`; //추모관
+export const memorial_application = `${protocol}${server}/memorial_application`; //추모관 신청
+export const anime = `${protocol}${server}/anime`; //에니메이션
