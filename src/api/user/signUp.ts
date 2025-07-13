9c055eed60a5ebc51f6bbf5cdb5b56544fe90bed
winshine0326 <shane10340@gmail.com>
2025-07-14 03:14:51 +0900

    feat :: 로그인 시 아이디도 입력 받기

--- src/api/user/signUp.ts
+++ src/api/user/signUp.ts
@@ -1,42 +1,49 @@
-import axios, {AxiosResponse} from "axios";
-import {user} from "@/config";
-import {useMutation} from "@tanstack/react-query";
+import axios, { AxiosResponse } from 'axios';
+import { user } from '@/config';
+import { useMutation } from '@tanstack/react-query';
 interface SignUpParams {
-    name: string;
-    email: string;
-    pw: string;
-    changeToLogIn: () => void;
+  name: string;
+  userId: string;
+  email: string;
+  pw: string;
+  changeToLogIn: () => void;
 }
 
-export const signUp = async ({ name, email, pw, changeToLogIn }: SignUpParams):Promise<boolean> => {
-    const data = {
-        userId: email.split('@')[0],
-        email,
-        name,
-        password: pw
-    };
-    try {
-        const response: AxiosResponse = await axios.post(`${user}/register`,data, {
-            withCredentials: true,
-            headers: { 'Content-Type': 'application/json' },
-        });
-        console.log(JSON.stringify(response.data));
-        alert("회원가입이 완료되었습니다.");
-        changeToLogIn();
-        return true;
-    }catch(error: any) {
-        console.error(error);
-        if (error.response?.data) {
-            alert(`회원가입 실패: 다시 시도해 주세요!!`);
-            console.log(`회원가입 실패: ${JSON.stringify(error.response.data)}`);
-        } else {
-            alert("회원가입 중 오류가 발생했습니다.");
-        }
-        return false;
-        }
+export const signUp = async ({
+  name,
+  userId,
+  email,
+  pw,
+  changeToLogIn,
+}: SignUpParams): Promise<boolean> => {
+  const data = {
+    userId,
+    email,
+    name,
+    password: pw,
+  };
+  try {
+    const response: AxiosResponse = await axios.post(`${user}/register`, data, {
+      withCredentials: true,
+      headers: { 'Content-Type': 'application/json' },
+    });
+    console.log(JSON.stringify(response.data));
+    alert('회원가입이 완료되었습니다.');
+    changeToLogIn();
+    return true;
+  } catch (error: any) {
+    console.error(error);
+    if (error.response?.data) {
+      alert(`회원가입 실패: 다시 시도해 주세요!!`);
+      console.log(`회원가입 실패: ${JSON.stringify(error.response.data)}`);
+    } else {
+      alert('회원가입 중 오류가 발생했습니다.');
+    }
+    return false;
+  }
 };
 export const useSignUp = () => {
-    return useMutation({
-        mutationFn: signUp,
-    });
-}
+  return useMutation({
+    mutationFn: signUp,
+  });
+};
--- src/applications/utility/login/index.tsx
+++ src/applications/utility/login/index.tsx
@@ -1,9 +1,9 @@
 import * as _ from './style';
 import Logo from '@/assets/windeath44.svg';
-import Button from "@/applications/components/button";
-import Inputs from "@/applications/components/inputs";
+import Button from '@/applications/components/button';
+import Inputs from '@/applications/components/inputs';
 import { useAtomValue } from 'jotai';
-import {useState} from "react";
+import { useState } from 'react';
 import { useLogIn } from '@/api/auth/logIn';
 import { taskTransformerAtom } from '@/atoms/taskTransformer';
 
@@ -12,50 +12,69 @@ type Props = {
   changeToSignUp: () => void;
   changeToEmailCheck: () => void;
 };
-const LogIn = ({ setIsLogIned, changeToSignUp , changeToEmailCheck}: Props) => {
-  const [userId, setUserId] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+const LogIn = ({ setIsLogIned, changeToSignUp, changeToEmailCheck }: Props) => {
+  const [userId, setUserId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const logInMutation = useLogIn();
   const taskTransform = useAtomValue(taskTransformerAtom);
   const inputList = [
-    { label: "이메일:", value: userId, setValue: setUserId, type: "text" },
-    { label: "비밀번호:", value: password, setValue: setPassword, type: "password" },
+    { label: '아이디:', value: userId, setValue: setUserId, type: 'text' },
+    { label: '비밀번호:', value: password, setValue: setPassword, type: 'password' },
   ];
   const checkLogIn = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const id = userId.split('@')[0]
-    logInMutation.mutate({ id, password }, {
-          onSuccess: () => {
-            setIsLogIned(true);
-            taskTransform?.('LogIn', '');
-          }
-        }
+    const id = userId;
+    logInMutation.mutate(
+      { id, password },
+      {
+        onSuccess: () => {
+          setIsLogIned(true);
+          taskTransform?.('LogIn', '');
+        },
+        onError: (error) => {
+          console.error('로그인 실패', error);
+        },
+      },
     );
   };
-    return (
-      <_.tempMain>
-        <_.tempImage>
-          <img src={Logo} alt="" />
-        </_.tempImage>
-        <_.tempBulk />
-        <_.tempMainStyle>
-          <_.tempInputs>
-            {inputList.map((item) => (
-                <Inputs  {...item}/>
-            ))}
-          </_.tempInputs>
-          <_.tempButtons>
-            <Button props="확인" onClick={checkLogIn}/>
-            <Button props="취소" onClick={() => {
+  return (
+    <_.tempMain>
+      <_.tempImage>
+        <img
+          src={Logo}
+          alt=""
+        />
+      </_.tempImage>
+      <_.tempBulk />
+      <_.tempMainStyle>
+        <_.tempInputs>
+          {inputList.map((item) => (
+            <Inputs {...item} />
+          ))}
+        </_.tempInputs>
+        <_.tempButtons>
+          <Button
+            props="확인"
+            onClick={checkLogIn}
+          />
+          <Button
+            props="취소"
+            onClick={() => {
               setIsLogIned(true);
               taskTransform?.('LogIn', '');
-            }}/>
-            <Button props="비밀번호 찾기" onClick={() => changeToEmailCheck()}/>
-            <Button props="회원가입" onClick={() => changeToSignUp()}/>
-          </_.tempButtons>
-        </_.tempMainStyle>
-      </_.tempMain>
-    )
-
-}
+            }}
+          />
+          <Button
+            props="비밀번호 찾기"
+            onClick={() => changeToEmailCheck()}
+          />
+          <Button
+            props="회원가입"
+            onClick={() => changeToSignUp()}
+          />
+        </_.tempButtons>
+      </_.tempMainStyle>
+    </_.tempMain>
+  );
+};
 export default LogIn;
