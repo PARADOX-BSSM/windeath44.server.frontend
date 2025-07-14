095791f4f443385cfa49063baa19b1198003d9ea
soeun823 <bsoeun866@gmail.com>
2025-07-14 14:18:45 +0900

    chore :: [FRONTEND-82] 수정

--- src/api/user/resetUserPassword.ts
+++ src/api/user/resetUserPassword.ts
@@ -20,7 +20,7 @@ const resetPassword = async ({ email, pw }: ResetPasswordParams): Promise<boolea
     console.error(error);
     if (error.response?.data) {
       alert(`비밀번호 재설정 실패: 다시 시도해 주세요!!`);
-      console.log(`비밀번호 재설정 실패: ${JSON.stringify(error.response.data)}`);
+      console.error('비밀번호 재설정 실패:', error.response.status);
     } else {
       alert('비밀번호 재설정 중 오류가 발생했습니다.');
     }
