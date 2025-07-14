52a113858ebc1834f3fa74e78319cc3b16e9e618
winshine0326 <shane10340@gmail.com>
2025-07-14 20:06:08 +0900

    feat :: 추모관 신청 시 유저 아이디 표시

--- src/api/memorial/applyMemorial.ts
+++ src/api/memorial/applyMemorial.ts
@@ -3,25 +3,46 @@ import { useMutation } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 import { memorial_application } from '@/config/index';
 
-// export const applyMemorial = async (): Promise<boolean> => {
-//   try {
-//     const response: AxiosResponse = await axios.post(
-//       `${memorial_application}/email`,
-//       { email },
-//       {
-//         withCredentials: true,
-//         headers: { 'Content-Type': 'application/json' },
-//       },
-//     );
-//     console.log(JSON.stringify(response.data));
-//     return true;
-//   } catch (error: any) {
-//     if (error.response?.data) {
-//       alert(`이메일 전송 실패: 다시 입력해 주세요!`);
-//       console.log(`이메일 전송 실패: ${JSON.stringify(error.response.data)}`);
-//     } else {
-//       alert('이메일 전송 중 오류가 발생했습니다.');
-//     }
-//     throw error;
-//   }
-// };
+interface ApplyMemorialInterface {
+  characterId: string;
+  content: string;
+}
+
+const applyMemorial = async ({
+  characterId,
+  content,
+}: ApplyMemorialInterface): Promise<boolean> => {
+  const data = {
+    characterId: characterId,
+    content: content,
+  };
+  try {
+    const response: AxiosResponse = await axios.post(`${memorial_application}/email`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+      withCredentials: true,
+    });
+    console.log(JSON.stringify(response.data));
+    return true;
+  } catch (error: any) {
+    if (error.response?.data) {
+      alert(`추모관 등록 오류`);
+      console.log(`이메일 전송 실패: ${JSON.stringify(error.response.data)}`);
+    } else {
+      alert('추모관 등록 중 오류가 발생했습니다.');
+    }
+    throw error;
+  }
+};
+
+export const useApplyMemorial = () => {
+  return useMutation({
+    mutationFn: applyMemorial,
+    onSuccess: () => {
+      alert('추모관이 성공적으로 등록되었습니다.');
+    },
+    onError: () => {},
+  });
+};
