76a9a02568d75f20a356624984c814ef21fe8cda
soeun823 <bsoeun866@gmail.com>
2025-07-14 13:56:03 +0900

    feat :: [FRONTEND-82] 비밀번호 재설정 api

---
+++ src/api/user/resetUserPassword.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { user } from '@/config';
+import { useMutation } from '@tanstack/react-query';
+interface SignUpParams {
+  email: string;
+  pw: string;
+}
+const resetPassword = async ({ email, pw }: SignUpParams): Promise<boolean> => {
+  const data = {
+    email,
+    password: pw,
+  };
+  try {
+    await axios.patch(`${user}/retrieve/password`, data, {
+      withCredentials: true,
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return true;
+  } catch (error: any) {
+    console.error(error);
+    if (error.response?.data) {
+      alert(`비밀번호 재설정 실패: 다시 시도해 주세요!!`);
+      console.log(`비밀번호 재설정 실패: ${JSON.stringify(error.response.data)}`);
+    } else {
+      alert('비밀번호 재설정 중 오류가 발생했습니다.');
+    }
+    throw error;
+  }
+};
+export const useResetPassword = () => {
+  return useMutation({
+    mutationFn: resetPassword,
+  });
+};
--- src/applications/utility/passwordChange/index.tsx
+++ src/applications/utility/passwordChange/index.tsx
@@ -2,13 +2,27 @@ import Button from '@/applications/components/button';
 import * as _ from './style';
 import Logo from '@/assets/windeath44.svg';
 import Inputs from '@/applications/components/inputs';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useResetPassword } from '@/api/user/resetUserPassword.ts';
 interface Props {
   changeToLogIn: () => void;
 }
 const PasswordChange = ({ changeToLogIn }: Props) => {
   const [pw, setPw] = useState<string>('');
   const [checkingPw, setCheckingPw] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const mutateResetPassword = useResetPassword();
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    mutateResetPassword.mutate(
+      { email, pw },
+      {
+        onSuccess: () => {
+          changeToLogIn();
+        },
+      },
+    );
+  };
   return (
     <_.tempMain>
       <_.tempImageStyle>
@@ -20,6 +34,14 @@ const PasswordChange = ({ changeToLogIn }: Props) => {
       <_.tempBulkStyle />
       <_.tempMainStyle>
         <_.tempInputsStyle>
+          <Inputs
+            label={'이메일:'}
+            value={email}
+            setValue={setEmail}
+            type={'text'}
+            width={'70%'}
+            flex={true}
+          />
           <Inputs
             label={'비밀번호:'}
             value={pw}
@@ -39,7 +61,7 @@ const PasswordChange = ({ changeToLogIn }: Props) => {
         </_.tempInputsStyle>
         <_.tempButtonsStyle>
           <Button
-            onClick={changeToLogIn}
+            onClick={handleSubmit}
             props="확인"
           />
         </_.tempButtonsStyle>
