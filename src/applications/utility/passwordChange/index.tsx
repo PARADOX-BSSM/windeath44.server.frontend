13ba8d7796db8ec234302033f20ba0948f6ce086
soeun823 <bsoeun866@gmail.com>
2025-07-14 13:35:34 +0900

    feat :: [FRONTEND-82] 스타일 구현

--- src/applications/utility/passwordChange/index.tsx
+++ src/applications/utility/passwordChange/index.tsx
@@ -1,16 +1,50 @@
 import Button from '@/applications/components/button';
+import * as _ from './style';
+import Logo from '@/assets/windeath44.svg';
+import Inputs from '@/applications/components/inputs';
+import { useState } from 'react';
 interface Props {
   changeToLogIn: () => void;
 }
 const PasswordChange = ({ changeToLogIn }: Props) => {
+  const [pw, setPw] = useState<string>('');
+  const [checkingPw, setCheckingPw] = useState<string>('');
   return (
-    <div>
-      <div>비밀번호 변경</div>
-      <Button
-        onClick={changeToLogIn}
-        props="확인"
-      />
-    </div>
+    <_.tempMain>
+      <_.tempImageStyle>
+        <img
+          src={Logo}
+          alt="logo"
+        />
+      </_.tempImageStyle>
+      <_.tempBulkStyle />
+      <_.tempMainStyle>
+        <_.tempInputsStyle>
+          <Inputs
+            label={'비밀번호:'}
+            value={pw}
+            setValue={setPw}
+            type={'password'}
+            width={'70%'}
+            flex={true}
+          />
+          <Inputs
+            label={'비밀번호 재입력:'}
+            value={checkingPw}
+            setValue={setCheckingPw}
+            type={'password'}
+            width={'70%'}
+            flex={true}
+          />
+        </_.tempInputsStyle>
+        <_.tempButtonsStyle>
+          <Button
+            onClick={changeToLogIn}
+            props="확인"
+          />
+        </_.tempButtonsStyle>
+      </_.tempMainStyle>
+    </_.tempMain>
   );
 };
 export default PasswordChange;
---
+++ src/applications/utility/passwordChange/style.ts
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+export const tempMain = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const tempImageStyle = styled.div`
+  width: 100%;
+  height: 180px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  img {
+    width: 100%;
+  }
+`;
+export const tempBulkStyle = styled.div`
+  width: 100%;
+  height: 10px;
+  background-color: #ffbbf5;
+`;
+export const tempMainStyle = styled.div`
+  background-color: #ffd3fb;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+export const tempInputsStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
+  padding: 20px;
+`;
+export const tempButtonsStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  padding: 20px;
+  gap: 10px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
