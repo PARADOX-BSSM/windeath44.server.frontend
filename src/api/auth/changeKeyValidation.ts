969309ac74c467e0bdf45574181971c10a83b32f
soeun823 <bsoeun866@gmail.com>
2025-07-14 10:19:22 +0900

    chore :: [FRONTEND-74] 내용 수정

--- src/api/auth/changeKeyValidation.ts
+++ src/api/auth/changeKeyValidation.ts
@@ -17,10 +17,10 @@ const changeKeyValidation = async ({ authorizationCode }: ChangeKeyValidation) =
   } catch (error: any) {
     console.error(error);
     if (error.response?.data) {
-      alert(` 실패: 다시 시도해 주세요!!`);
-      console.log(`이메일 전송 실패: ${JSON.stringify(error.response.data)}`);
+      alert(`인증 코드 검증 실패: 다시 시도해 주세요!!`);
+      console.log(`인증 코드 검증 실패: ${JSON.stringify(error.response.data)}`);
     } else {
-      alert('이메일 전송 중 오류가 발생했습니다.');
+      alert('인증 코드 검증 중 오류가 발생했습니다.');
     }
     return false;
   }
