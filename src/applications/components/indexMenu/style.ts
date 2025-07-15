3dcfc08055205b6122eada8750e21af1fb958ce3
Roena0516 <tkf2001.yc@gmail.com>
2025-07-16 00:20:06 +0900

    fix :: 메모리얼 스타일 수정

--- src/applications/components/indexMenu/style.ts
+++ src/applications/components/indexMenu/style.ts
@@ -1,18 +1,18 @@
 import styled from '@emotion/styled';
 
 export const IndexContainer = styled.div`
-    display: flex;
-    padding: 0px 16px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 20px;
-`
+  display: flex;
+  padding: 0 1rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.25rem;
+`;
 
 export const IndexText = styled.p`
-    color: var(--Black, #2E2E2E);
-    font-family: Galmuri11;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-`
+  color: var(--Black, #2e2e2e);
+  font-family: Galmuri11;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
