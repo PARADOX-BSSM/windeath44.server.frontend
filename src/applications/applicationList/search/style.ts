d414dd709c72562c26c8c02cbaef7dd0665c43d5
Roena0516 <tkf2001.yc@gmail.com>
2025-07-15 10:38:55 +0900

    fix :: 메인 스타일 변경

--- src/applications/applicationList/search/style.ts
+++ src/applications/applicationList/search/style.ts
@@ -1,13 +1,15 @@
 import styled from '@emotion/styled';
+
 export const main = styled.div`
   box-sizing: border-box;
   background-color: var(--light-primary-color);
   font-family: 'Galmuri11';
-  font-size: 16px;
+  font-size: 1rem;
   width: 100%;
   height: 100%;
-  padding: 12px 24px;
+  padding: 0.65rem 1rem;
 `;
+
 export const main_serve = styled.div`
   box-sizing: border-box;
   width: 100%;
@@ -15,32 +17,33 @@ export const main_serve = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  gap: 4px;
+  gap: 0.25rem;
 `;
+
 export const search_task = styled.div<{ isColumn: boolean }>`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: ${({ isColumn }) => (isColumn ? 'column' : 'row')};
-  gap: ${({ isColumn }) => (isColumn ? '0px' : '8px')};
+  gap: ${({ isColumn }) => (isColumn ? '0' : '0.5rem')};
 `;
-// *개체
+
 export const object = styled.div`
-  border-width: 1.5px 0 0 1.5px;
+  border-width: 0.094rem 0 0 0.094rem;
   border-style: solid;
   border-color: #808080;
   box-sizing: border-box;
   & > div {
     width: 100%;
     height: 100%;
-    border-width: 0 1.5px 1.5px 0;
+    border-width: 0 0.094rem 0.094rem 0;
     border-style: solid;
     border-color: #ffffff;
     box-sizing: border-box;
     background-color: var(--dark-primary-color);
     display: flex;
-    gap: 4px;
-    padding-left: 4px;
+    gap: 0.25rem;
+    padding-left: 0.25rem;
     & > div {
       margin: auto 0;
     }
