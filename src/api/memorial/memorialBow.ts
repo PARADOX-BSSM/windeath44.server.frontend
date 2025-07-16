8c5dc152049c9daabfc41c1c87d1403bc71c6432
soeun823 <bsoeun866@gmail.com>
2025-07-17 08:19:38 +0900

    feat :: [FRONTEND-100] memorial bows api 작성

---
+++ src/api/memorial/memorialBow.ts
@@ -0,0 +1,31 @@
+import { useMutation } from '@tanstack/react-query';
+import { memorial } from '@/config';
+import axios, { AxiosResponse } from 'axios';
+
+const memorialBow = async ({ userId }) => {
+  try {
+    const response: AxiosResponse = await axios.post(`${memorial}/bow`, userId, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+        'Content-Type': 'application/json',
+      },
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (err) {
+    console.error('절하기 값 불러오는 중 오류:', err);
+    throw err;
+  }
+};
+
+export const useMemorialBow = () => {
+  return useMutation({
+    mutationFn: memorialBow,
+    onSuccess: (data: any) => {
+      console.log(data);
+    },
+    onError: (error: any) => {
+      console.log(error);
+    },
+  });
+};
