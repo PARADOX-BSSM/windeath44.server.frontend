2b1685b1b7d4bfed3959c6bc7d58077b98f3c15b
soeun823 <bsoeun866@gmail.com>
2025-07-16 21:55:25 +0900

    refactor :: [FRONTEND-99] prettier 적용

--- src/applications/applicationList/settings/index.tsx
+++ src/applications/applicationList/settings/index.tsx
@@ -1,31 +1,31 @@
-import {useEffect} from "react";
+import { useEffect } from 'react';
 import { useAtomValue } from 'jotai';
-import {useStack} from '@/hooks/dataStructure.tsx'
-import { taskSearchAtom } from "@/atoms/taskTransformer.ts";
+import { useStack } from '@/hooks/dataStructure.tsx';
+import { taskSearchAtom } from '@/atoms/taskTransformer.ts';
 
 interface MemorialApproachProps {
   window: React.CSSProperties;
   setWindow: React.Dispatch<React.SetStateAction<React.CSSProperties>>;
-  setUpHeight: number,
-  setUpWidth: number,
+  setUpHeight: number;
+  setUpWidth: number;
 }
 
-const MemorialApproach = ({ window, setWindow, setUpHeight, setUpWidth }: MemorialApproachProps) => {
+const MemorialApproach = ({
+  window,
+  setWindow,
+  setUpHeight,
+  setUpWidth,
+}: MemorialApproachProps) => {
   const [stack, push, pop, top] = useStack(window, setWindow, setUpHeight, setUpWidth);
   const taskSearch = useAtomValue(taskSearchAtom);
 
   useEffect(() => {
-    console.log("stack: ", stack);
-    console.log("top: ", top());
+    // console.log("stack: ", stack);
+    // console.log("top: ", top());
   }, [stack]);
   useEffect(() => {
-    push(taskSearch?.("memorialMenu", stack, push, pop, top));
-  }, [])
-    return (
-      <>
-        {top()?.component}
-      </>
-    )
-
-}
+    push(taskSearch?.('memorialMenu', stack, push, pop, top));
+  }, []);
+  return <>{top()?.component}</>;
+};
 export default MemorialApproach;
