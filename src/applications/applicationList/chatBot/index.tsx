88b0b556688dcbc94d211bb28b1ace006a632e06
winshine0326 <shane10340@gmail.com>
2025-08-25 20:24:23 +0900

    feat :: 채팅 시 글 추가 기능

--- src/applications/applicationList/chatBot/index.tsx
+++ src/applications/applicationList/chatBot/index.tsx
@@ -1,19 +1,61 @@
 import { useState } from 'react';
 import * as _ from './style';
+import ChatMessage from '@/applications/components/chatMessage';
 import Choten from '@/assets/profile/choten.svg';
 import Ame from '@/assets/profile/ame.svg';
 import Hosino from '@/assets/character/hosino.svg';
 
+interface Message {
+  id: string;
+  avatar: string;
+  author: string;
+  handle?: string;
+  text: string;
+}
+
 const ChatBot = () => {
   const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      id: '1',
+      avatar: Ame,
+      author: '로에나',
+      handle: '@roena0516',
+      text: '1화만에 죽은 소감이 어때?',
+    },
+    {
+      id: '2',
+      avatar: Hosino,
+      author: '호시노 아이',
+      text: '뒤질래?',
+    },
+    {
+      id: '3',
+      avatar: Ame,
+      author: '로에나',
+      handle: '@roena0516',
+      text: 'ㅜㅜㅜㅜㅜ',
+    },
+  ]);
+
+  const addMessage = () => {
+    if (!message.trim()) return;
+
+    const newMessage: Message = {
+      id: Date.now().toString(),
+      avatar: Ame,
+      author: '사용자',
+      handle: '@user',
+      text: message.trim(),
+    };
+
+    setMessages((prev) => [...prev, newMessage]);
+    setMessage('');
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!message.trim()) return;
-
-    // TODO: Implement message sending logic
-    console.log('Sending message:', message);
-    setMessage('');
+    addMessage();
   };
 
   const handleMemorialClick = () => {
@@ -70,37 +112,15 @@ const ChatBot = () => {
         <_.RightPanel>
           <_.ChatArea>
             <_.ChatMessagesContainer>
-              <_.ChatMessage>
-                <_.MessageAvatar src={Ame} />
-                <_.MessageContent>
-                  <_.MessageHeader>
-                    <_.MessageAuthor>로에나</_.MessageAuthor>
-                    <_.MessageHandle>@roena0516</_.MessageHandle>
-                  </_.MessageHeader>
-                  <_.MessageText>1화만에 죽은 소감이 어때?</_.MessageText>
-                </_.MessageContent>
-              </_.ChatMessage>
-
-              <_.ChatMessage>
-                <_.MessageAvatar src={Hosino} />
-                <_.MessageContent>
-                  <_.MessageHeader>
-                    <_.MessageAuthor>호시노 아이</_.MessageAuthor>
-                  </_.MessageHeader>
-                  <_.MessageText>뒤질래?</_.MessageText>
-                </_.MessageContent>
-              </_.ChatMessage>
-
-              <_.ChatMessage>
-                <_.MessageAvatar src={Ame} />
-                <_.MessageContent>
-                  <_.MessageHeader>
-                    <_.MessageAuthor>로에나</_.MessageAuthor>
-                    <_.MessageHandle>@roena0516</_.MessageHandle>
-                  </_.MessageHeader>
-                  <_.MessageText>ㅜㅜㅜㅜㅜ</_.MessageText>
-                </_.MessageContent>
-              </_.ChatMessage>
+              {messages.map((msg) => (
+                <ChatMessage
+                  key={msg.id}
+                  avatar={msg.avatar}
+                  author={msg.author}
+                  handle={msg.handle}
+                  text={msg.text}
+                />
+              ))}
             </_.ChatMessagesContainer>
           </_.ChatArea>
 
@@ -114,8 +134,8 @@ const ChatBot = () => {
               />
             </_.InputForm>
             <_.SendButton
-              type="submit"
-              form="chatForm"
+              type="button"
+              onClick={addMessage}
             >
               전송
             </_.SendButton>
--- src/applications/applicationList/chatBot/style.ts
+++ src/applications/applicationList/chatBot/style.ts
@@ -161,56 +161,6 @@ export const ChatMessagesContainer = styled.div`
   overflow: hidden;
 `;
 
-export const ChatMessage = styled.div`
-  background: var(--chatbot-white);
-  padding: 12px 15px;
-  display: flex;
-  gap: 9px;
-  align-items: flex-start;
-`;
-
-export const MessageAvatar = styled.div<{ src: string }>`
-  width: 45px;
-  height: 45px;
-  background-image: url(${(props) => props.src});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  flex-shrink: 0;
-`;
-
-export const MessageContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 3px;
-  padding: 0 7px;
-`;
-
-export const MessageHeader = styled.div`
-  display: flex;
-  align-items: flex-end;
-  gap: 8px;
-  padding: 5px 0;
-`;
-
-export const MessageAuthor = styled.span`
-  font-family: 'Galmuri11', sans-serif;
-  font-size: 12px;
-  color: var(--chatbot-author);
-`;
-
-export const MessageHandle = styled.span`
-  font-family: 'Galmuri11', sans-serif;
-  font-size: 10px;
-  color: var(--dark-primary-color);
-`;
-
-export const MessageText = styled.p`
-  font-family: 'Galmuri11', sans-serif;
-  font-size: 12px;
-  color: var(--primary-black);
-  margin: 0;
-`;
 
 export const InputSection = styled.div`
   display: flex;
