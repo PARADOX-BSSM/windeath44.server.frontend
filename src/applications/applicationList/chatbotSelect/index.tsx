ff566e15565b0acd924f336984b10bf9ecb19ba1
soeun823 <bsoeun866@gmail.com>
2025-08-26 18:56:22 +0900

    feat :: [FRONTEND-116] 퍼블리싱 완료

--- src/applications/applicationList/chatbotSelect/index.tsx
+++ src/applications/applicationList/chatbotSelect/index.tsx
@@ -1,13 +1,30 @@
 import * as _ from './style.ts';
 import Inputs from '@/applications/components/inputs';
 import { useState } from 'react';
+import { data } from './data.ts';
 
 const ChatbotSelect = () => {
   const [inputs, setInputs] = useState<string>('');
 
   return (
     <_.Container>
-      <_.TopContainer></_.TopContainer>
+      <_.TopContainer>
+        {data?.map((item) => (
+          <_.TopContainerItem>
+            <_.TopContainerItemInfo>
+              <_.TopContainerItemImage
+                src={item.img}
+                alt={`${item.name}의 사진`}
+              />
+              <_.TopContainerItemText>
+                <_.TopContainerItemTitle>{item.name}</_.TopContainerItemTitle>
+                <_.TopContainerItemDesc>{item.descript}</_.TopContainerItemDesc>
+              </_.TopContainerItemText>
+            </_.TopContainerItemInfo>
+            <_.TopContainerSubmit>학습</_.TopContainerSubmit>
+          </_.TopContainerItem>
+        ))}
+      </_.TopContainer>
       <_.BottomContainer>
         <Inputs
           width="80%"
--- src/applications/applicationList/chatbotSelect/style.ts
+++ src/applications/applicationList/chatbotSelect/style.ts
@@ -15,9 +15,11 @@ export const Container = styled.div`
 export const TopContainer = styled.div`
   width: 100%;
   height: 100%;
+  padding: 2px;
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: flex-start;
   background-color: var(--very-light-primary-color);
   box-shadow:
     -1px -1px 0 0 #fff inset,
@@ -36,6 +38,59 @@ export const BottomContainer = styled.div`
 export const BottomContainerSubmit = styled.button`
   width: 20%;
   height: 100%;
+  color: var(--primary-black);
+  font-family: Galmuri11;
+  font-size: 1em;
+  background-color: var(--light-primary-color);
+  box-shadow:
+    -1px -1px 0 0 var(--primary-black) inset,
+    1px 1px 0 0 #fff inset,
+    -2px -2px 0 0 var(--dark-primary-color) inset,
+    2px 2px 0 0 var(--secondary-color) inset;
+  border: none;
+`;
+export const TopContainerItem = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 5.5rem;
+  padding: 0.75rem 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  box-shadow: 0 -1px 0 0 #cccccc inset;
+  //border: 0 0 1px 0 var(--primary-black) solid;
+`;
+export const TopContainerItemInfo = styled.div`
+  display: flex;
+`;
+export const TopContainerItemImage = styled.img`
+  width: 4rem;
+  height: 4rem;
+  object-fit: cover;
+  box-sizing: border-box;
+`;
+export const TopContainerItemText = styled.div`
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.25rem;
+`;
+export const TopContainerItemTitle = styled.div`
+  color: #9a5a95;
+  font-family: Galmuri11;
+  font-size: 0.95rem;
+`;
+export const TopContainerItemDesc = styled.div`
+  color: var(--primary-black);
+  font-family: Galmuri11;
+  font-size: 0.8rem;
+`;
+export const TopContainerSubmit = styled.button`
+  width: 16%;
+  height: fit-content;
+  padding: 0.35rem 0;
   color: var(--primary-black);
   font-family: Galmuri11;
   font-size: 1em;
