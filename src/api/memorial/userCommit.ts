d9c44b4528946a8532acc69569b583d70dcd9af3
soeun823 <bsoeun866@gmail.com>
2025-09-05 16:18:07 +0900

    feat :: [FRONTEND-123] user의 commit에  query 구현

--- src/api/memorial/userCommit.ts
+++ src/api/memorial/userCommit.ts
@@ -1,17 +1,34 @@
 import api from '@/api/axiosInstance.ts';
 import { memorial } from '@/config';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 // user의 memorial commit 사항을 db에 저장하는 api
 const postMemorialCommit = async (data, userId): Promise<> => {
-  const data = data;
-  const response = await api.get(`:${memorial}/commit`, {
-    headers: {
-      'user-id': userId,
-      'Content-Type': 'application/json',
-    },
-  });
-  return response.data;
+  try {
+    const data = data;
+    const response = await api.get(`:${memorial}/commit`, {
+      headers: {
+        'user-id': userId,
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    console.log(`실패 : ${JSON.stringify(error.response.data)}`);
+    throw error;
+  }
 };
+
+export const usePostCommit = () => {
+  return useMutation({
+    mutationFn: postMemorialCommit,
+    onSuccess: (e) => {
+      console.log(e);
+    },
+    onError: () => {},
+  });
+};
+
 //user의 memorial pull request 사항을 저장하는 api
 const postMemorialPullRequest = async (memorialPullRequestId, userId): Promise<> => {
   const memorialPullRequestId = memorialPullRequestId;
@@ -23,12 +40,35 @@ const postMemorialPullRequest = async (memorialPullRequestId, userId): Promise<>
   });
   return response.data;
 };
+
+export const usePostPullRequest = () => {
+  return useMutation({
+    mutationFn: postMemorialPullRequest,
+    onSuccess: (e) => {
+      console.log(e);
+    },
+    onError: () => {},
+  });
+};
 //get commits
 const getMemorialCommits = async (memorialId): Promise<void> => {
   const response = await api.get(`:${memorial}commits/${memorialId}`);
   return response.data;
 };
+export const useGetCommitsQuery = ({ memorialId }) => {
+  return useQuery({
+    queryKey: [],
+    queryFn: () => getMemorialCommits({ memorialId }),
+  });
+};
 //get commit by commitId
-const getMemorialCommits = async (commitId): Promise<void> => {
+const getCommitById = async (commitId): Promise<void> => {
   const response = await api.get(`:${memorial}commits/id/${commitId}`);
   return response.data;
+};
+export const useGetCommitsByIdQuery = ({ commitId }) => {
+  return useQuery({
+    queryKey: [],
+    queryFn: () => getCommitById({ commitId }),
+  });
+};
