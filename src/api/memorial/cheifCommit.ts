4d363ef582210abcbddbd1db9f23d2092e645c45
soeun823 <bsoeun866@gmail.com>
2025-09-05 16:38:14 +0900

    feat :: [FRONTEND-123] chief의 commit에  query 구현

--- src/api/memorial/cheifCommit.ts
+++ src/api/memorial/cheifCommit.ts
@@ -1,5 +1,6 @@
 import api from '@/api/axiosInstance.ts';
 import { memorial } from '@/config';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 // chief mergeable
 const postMemorialMergeable = async (memorialPullRequestId, userId): Promise<> => {
@@ -12,6 +13,16 @@ const postMemorialMergeable = async (memorialPullRequestId, userId): Promise<> =
   });
   return response.data;
 };
+
+export const usePostMergeable = () => {
+  return useMutation({
+    mutationFn: postMemorialMergeable,
+    onSuccess: (e) => {
+      console.log(e);
+    },
+    onError: () => {},
+  });
+};
 // chief merge
 const postMemorialMerge = async (memorialPullRequestId, userId): Promise<> => {
   const data = memorialPullRequestId;
@@ -22,6 +33,15 @@ const postMemorialMerge = async (memorialPullRequestId, userId): Promise<> => {
     },
   });
   return response.data;
+};
+export const usePatchMerge = () => {
+  return useMutation({
+    mutationFn: postMemorialMerge,
+    onSuccess: (e) => {
+      console.log(e);
+    },
+    onError: () => {},
+  });
 };
 // chief resolve
 const postMemorialResolve = async (memorialPullRequestId, userId): Promise<> => {
@@ -36,6 +56,15 @@ const postMemorialResolve = async (memorialPullRequestId, userId): Promise<> => 
   });
   return response.data;
 };
+export const usePatchMemorialResolve = () => {
+  return useMutation({
+    mutationFn: postMemorialResolve,
+    onSuccess: (e) => {
+      console.log(e);
+    },
+    onError: () => {},
+  });
+};
 // chief reject
 const postMemorialReject = async (memorialPullRequestId, userId): Promise<> => {
   const data = memorialPullRequestId;
@@ -47,13 +76,34 @@ const postMemorialReject = async (memorialPullRequestId, userId): Promise<> => {
   });
   return response.data;
 };
+export const usePatchMemorialReject = () => {
+  return useMutation({
+    mutationFn: postMemorialReject,
+    onSuccess: (e) => {
+      console.log(e);
+    },
+    onError: () => {},
+  });
+};
 // chief get prs by memorial id
 const getPullRequestsByMemorialId = async (memorialId): Promise<> => {
   const response = await api.patch(`:${memorial}/pull-requests/${memorialId}`);
   return response.data;
 };
+export const useGetPrsQuery = ({ memorialId }) => {
+  return useQuery({
+    queryKey: [],
+    queryFn: () => getPullRequestsByMemorialId({ memorialId }),
+  });
+};
 // chief get pr by request id
 const getPullRequestByRequestId = async (requestId): Promise<> => {
   const response = await api.patch(`:${memorial}/pull-request/${requestId}`);
   return response.data;
+};
+export const useGetPrQuery = ({ requestId }) => {
+  return useQuery({
+    queryKey: [],
+    queryFn: () => getPullRequestByRequestId({ requestId }),
+  });
 };
