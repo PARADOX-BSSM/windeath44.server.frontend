81a5dfb64ea70821bb6eedb286d977e1f4ccbd84
winshine0326 <shane10340@gmail.com>
2025-09-10 17:59:58 +0900

    feat :: 아케이드 어플리케이션 퍼블리싱

--- src/applications/applicationList/game/index.tsx
+++ src/applications/applicationList/game/index.tsx
@@ -1,53 +1,35 @@
 import * as _ from './style';
-
-import setting from '@/assets/appIcons/setting.svg';
+import React from 'react';
 import folder from '@/assets/search/folder.svg';
 import rythmGame from '@/assets/appIcons/piano.svg';
-import { setCursorImage, CURSOR_IMAGES } from '@/lib/setCursorImg';
+import game from '@/assets/appIcons/game.svg';
 
-interface AdminItem {
-  id: string;
-  icon: string;
-  label: string;
-}
+const gameItems = [
+  { icon: rythmGame, name: '리듬게임' },
+  { icon: game, name: '설까기' },
+];
 
-const GameApp = () => {
-  const adminItems: AdminItem[] = [
-    { id: '1', icon: rythmGame, label: '리듬게임' },
-    { id: '2', icon: setting, label: '설까기' },
-  ];
-
+const Game: React.FC = () => {
   return (
     <_.Container>
-      <_.MainContent>
-        <_.ItemsList>
-          {adminItems.map((item) => (
-            <_.ItemRow
-              key={item.id}
-              onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
-              onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
-            >
-              <_.ItemIcon
-                src={item.icon}
-                alt={item.label}
-              />
-              <_.ItemLabel>{item.label}</_.ItemLabel>
-            </_.ItemRow>
+      <_.MainWindow>
+        <_.ItemContainer>
+          {gameItems.map((item, index) => (
+            <_.GameItem key={index}>
+              <_.GameIcon src={item.icon} />
+              <_.GameName>{item.name}</_.GameName>
+            </_.GameItem>
           ))}
-        </_.ItemsList>
-
+        </_.ItemContainer>
         <_.StatusBar>
           <_.StatusContent>
-            <_.StatusIcon
-              src={folder}
-              alt="folder"
-            />
-            <_.StatusText>{adminItems.length} 개체</_.StatusText>
+            <_.FolderIcon src={folder} />
+            <_.StatusText>{gameItems.length} 개체</_.StatusText>
           </_.StatusContent>
         </_.StatusBar>
-      </_.MainContent>
+      </_.MainWindow>
     </_.Container>
   );
 };
 
-export default GameApp;
+export default Game;
--- src/applications/applicationList/game/style.ts
+++ src/applications/applicationList/game/style.ts
@@ -1,95 +1,104 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+  display: flex;
   width: 100%;
   height: 100%;
-  background-color: var(--light-primary-color);
-  padding: 8px;
+  background-color: #ffd3fb;
+  padding: 0.125rem;
   box-sizing: border-box;
 `;
 
-export const MainContent = styled.div`
+export const MainWindow = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
+  background-color: #ffd3fb;
+  gap: 0.25rem;
 `;
 
-export const ItemsList = styled.div`
+export const ItemContainer = styled.div`
   flex: 1;
-  background: var(--chatbot-white);
+  background-color: white;
+  padding: 1rem;
   display: flex;
-  flex-direction: column;
-  padding: 2px 2px;
+  flex-wrap: wrap;
+  gap: 1.35rem;
+  overflow: hidden;
+  align-items: flex-start;
+  align-content: flex-start;
   box-shadow:
-    -1px -1px 0px 0px inset var(--chatbot-white),
-    1px 1px 0px 0px inset var(--primary-black),
-    -2px -2px 0px 0px inset var(--dark-primary-color),
-    2px 2px 0px 0px inset var(--dark-primary-color);
+    -1px -1px 0px 0px #ffffff inset,
+    1px 1px 0px 0px #2e2e2e inset,
+    -2px -2px 0px 0px #dcafdd inset,
+    2px 2px 0px 0px #dcafdd inset;
 `;
 
-export const ItemRow = styled.div`
+export const GameItem = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  gap: 8px;
-  padding: 4px 14px;
-  cursor: none;
+  justify-content: center;
+  width: 5.39rem;
+  gap: 0.563rem;
+  cursor: pointer;
 
   &:hover {
-    background-color: var(--very-light-primary-color);
+    opacity: 0.8;
   }
 `;
 
-export const ItemIcon = styled.img`
-  width: 36px;
-  height: 36px;
-  object-fit: cover;
+export const GameIcon = styled.img`
+  width: 3rem;
+  height: 3rem;
+  object-fit: contain;
 `;
 
-export const ItemLabel = styled.p`
+export const GameName = styled.div`
+  color: #2e2e2e;
   font-family: 'Galmuri11', sans-serif;
-  font-size: 18px;
-  color: var(--primary-black);
-  margin: 0;
-  line-height: 18.75px;
+  font-size: 1.031rem;
+  font-style: normal;
+  line-height: 1.125rem;
+  text-align: center;
+  min-width: 100%;
+  white-space: pre-line;
 `;
 
 export const StatusBar = styled.div`
-  height: 24px;
-  background: var(--dark-primary-color);
+  height: 1.5rem;
+  background-color: #dcafdd;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 4.5px;
-  box-shadow:
-    -1.5px -1.5px 0px 0px inset var(--chatbot-white),
-    1.5px 1.5px 0px 0px inset var(--stroke);
+  padding: 0 0.281rem;
   position: relative;
+  box-shadow:
+    -1.5px -1.5px 0px 0px #ffffff inset,
+    1.5px 1.5px 0px 0px #808080 inset;
 `;
 
 export const StatusContent = styled.div`
   display: flex;
   align-items: center;
-  gap: 4.5px;
+  gap: 0.281rem;
 `;
 
-export const StatusIcon = styled.img`
-  width: 20px;
-  height: 20px;
-  object-fit: cover;
+export const FolderIcon = styled.img`
+  width: 1.5rem;
+  height: 1.5rem;
 `;
 
-export const StatusText = styled.p`
+export const StatusText = styled.div`
+  color: #2e2e2e;
   font-family: 'Galmuri11', sans-serif;
-  font-size: 16.5px;
-  color: var(--primary-black);
-  margin: 0;
-  line-height: 15px;
+  font-size: 1.031rem;
+  line-height: 0.938rem;
+  white-space: pre;
 `;
 
 export const DragSizeIcon = styled.img`
-  width: 19.5px;
-  height: 19.5px;
-  object-fit: contain;
+  width: 1.219rem;
+  height: 1.219rem;
 `;
