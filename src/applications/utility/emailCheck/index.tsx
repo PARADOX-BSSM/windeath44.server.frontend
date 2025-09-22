ef2188bdd84b8f4470c8097c3ad6ac3283726d3f
Roena0516 <tkf2001.yc@gmail.com>
2025-09-22 15:01:14 +0900

    fix :: emailCheck setAlert으로 변경

--- src/applications/utility/emailCheck/index.tsx
+++ src/applications/utility/emailCheck/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import * as _ from '@/applications/utility/emailCheck/style.ts';
 import Logo from '@/assets/windeath44.svg';
+import Choten from '@/assets/profile/choten.svg';
 import Inputs from '@/applications/components/inputs';
 import { useChangeTemporaryKey } from '@/api/auth/changetemporaryKey.ts';
 import MemorialBtn from '@/applications/components/memorialBtn';
+import { useAtomValue } from 'jotai';
+import { alerterAtom } from '@/atoms/alerter';
+import { taskTransformerAtom } from '@/atoms/taskTransformer';
 
 interface Props {
   changeToLogIn: () => void;
@@ -13,7 +17,26 @@ interface Props {
 const EmailChack = ({ changeToLogIn, changeToAuth }: Props) => {
   const [email, setEmail] = useState('');
   const mutationChangeKey = useChangeTemporaryKey();
+
+  const setAlert = useAtomValue(alerterAtom);
+  const taskTransform = useAtomValue(taskTransformerAtom);
+
   const checkEmail = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (email.length === 0) {
+      setAlert?.(
+        Choten,
+        <>
+          이메일이 잘못되었습니다.
+          <br />
+          다시 입력해 주세요.
+        </>,
+        () => {
+          taskTransform?.('경고', '');
+        },
+      );
+      return;
+    }
+
     e.preventDefault();
     mutationChangeKey.mutate(
       { email },
@@ -25,8 +48,8 @@ const EmailChack = ({ changeToLogIn, changeToAuth }: Props) => {
     );
   };
 
-  const buttonWidth = "144px";
-  const buttonHeight = "42px";
+  const buttonWidth = '144px';
+  const buttonHeight = '42px';
   const buttonFontSize = '20px';
 
   return (
