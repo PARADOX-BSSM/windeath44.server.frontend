17c25eac3b086d126556eadfc118237cfbf30f33
bitbyte08 <me@bitworkspace.kr>
2025-09-26 00:51:07 +0900

    fix :: toUnixTime 함수의 인자값 검증

--- src/lib/toUnixTime.ts
+++ src/lib/toUnixTime.ts
@@ -1,4 +1,5 @@
-const toUnixTime = (date: string): number => {
+const toUnixTime = (date: string | undefined): number => {
+  if (date === undefined) throw new Error("date is undefined");
   const formatted = date.replace("/", "T") + ":00";
   const unixdate = new Date(formatted);
   if (isNaN(unixdate.getTime())) throw new Error("Invalid date format");
