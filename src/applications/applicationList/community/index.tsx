e7856180ad0402107fc7234b2b1b102308acde0a
harinsd404 <harinsd168@gmail.com>
2025-10-09 13:33:07 +0900

    feat::커뮤니티 상단 버튼 퍼블리싱

--- src/applications/applicationList/community/index.tsx
+++ src/applications/applicationList/community/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import * as _ from './style';
+import { useState } from 'react';
+import MemorialBtn from '@/applications/components/memorialBtn';
+import FilterBlock from '@/applications/components/filterBlock';
 
 const Community : React.FC = ()=>{
+    const [isOpen, setIsOpen] = useState(false);
+    const [sort, setSort] = useState("최신순");
+    const [active, setActive] = useState("humor");
+    const sortOp: string[] = ["최신순", "인기순"]
+    const sortChange = (value: deathType) => {
+        setSort(value);
+        setIsOpen(false);
+    };
+
     return(
         <_.Container>
+            <_.Main>
+                <_.Header>
+                    <_.ButtonArea>
+                        <MemorialBtn name="유머글" selected={active==="humor"} onClick={()=>setActive("humor")}
+                        type = 'menu' active={true} width="74px" height="100%" fontSize = '14px' />
+                        <MemorialBtn name="인기글" selected={active=="popular"} onClick={()=>setActive("popular")}
+                        type = 'menu' active={true} width="74px" height="100%" fontSize = '14px' />
+                        <MemorialBtn name="검색" selected={active=="serch"} onClick={()=>setActive("serch")}
+                        type = 'menu' active={true} width="74px" height="100%" fontSize = '14px' />
+                        <MemorialBtn name="게시글 작성" selected={false} onClick={()=>{}}
+                        type = 'menu' active={true} width="108px" height="100%" fontSize = '14px' />
+                    </_.ButtonArea>
+
+                    <_.sortInput>
+                        <FilterBlock
+                            label=""
+                            option={sort}
+                            isOpen={isOpen}
+                            onClick={()=> setIsOpen(!isOpen)}
+                            list={sortOp}
+                            onChange={sortChange}
+                        />
+                    </_.sortInput>
+                </_.Header>
+                
+            </_.Main>
+            <_.Judgement>
+                <_.NavJudgement>
+                    <_.JudgementImg />
+                    <_.JudgementText>재판으로</_.JudgementText>
+                </_.NavJudgement>
+            </_.Judgement>
         </_.Container>
     );
 }
--- src/applications/applicationList/community/style.ts
+++ src/applications/applicationList/community/style.ts
@@ -1,14 +1,72 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  display: flex;
-  padding: 12px;
-  width: 100%;
-  justify-content: center;
-  align-items: flex-start;
-  flex: 1 0 0;
-  align-self: stretch;
-  background: var(--LightPrimary, #ffd3fb);
-  height: 100%;
-  box-sizing: border-box;
+    display: flex;
+    padding: 20px;
+    align-items: flex-start;
+    gap: 20px;
+    flex: 1 0 0;
+    align-self: stretch;
+    background: var(--LightPrimary, #ffd3fb);
+    height: 100%;
+    box-sizing: border-box;
 `;
+export const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+    flex: 1 0 0;
+    align-self: stretch;
+`;
+export const Judgement = styled.div`
+    width: 132px;
+    height: 100%;
+    background: #ffd3fb;
+    border: none;
+    font-family: 'Galmuri11', sans-serif;
+    font-size: 20px;
+    color: var(--primary-black);
+    cursor: none;
+    box-shadow:
+        -1px -1px 0px 0px inset #2e2e2e,
+        1px 1px 0px 0px inset #ffffff,
+        -2px -2px 0px 0px inset #dcafdd,
+        2px 2px 0px 0px inset #ffbbf5;
+`;
+export const Header = styled.div`
+    display: flex;
+    height: 30px;
+    justify-content: space-between;
+    align-items: center;
+    align-self: stretch;
+`;
+export const ButtonArea = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 14px;
+`;
+export const sortInput = styled.div`
+    width: 90px;
+    font-family: 'Galmuri11', sans-serif;
+    font-size: 16px
+`
+
+
+export const NavJudgement = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+`
+export const JudgementImg = styled.image`
+`
+export const JudgementText = styled.p`
+    color: #2e2e2e;
+    text-align: center;
+    font-family: Galmuri11;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`
