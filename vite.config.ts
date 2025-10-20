d281c5d63af57dcf423caaf7f1c97ad82421b2cd
bitbyte08 <me@bitworkspace.kr>
2025-10-20 16:16:07 +0900

    feat :: reactCompiler 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import tsconfigPath from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import babelPlugin from 'vite-plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.gif'],
-  plugins: [react(), tsconfigPath()],
+  plugins: [react(),
+    babelPlugin({
+      babelConfig:{
+        plugins: ['babel-plugin-react-compiler']
+      }
+    }),
+    tsconfigPath()],
 });
