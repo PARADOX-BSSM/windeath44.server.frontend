70a61ee34cab3f1c7b17213cb11ef0dfbd436791
bitbyte08 <me@bitworkspace.kr>
2025-10-20 21:58:41 +0900

    fix :: React 19 이전 버전에 맞춰 config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import tsconfigPath from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
-import babelPlugin from 'vite-plugin-babel';
+
+const ReactCompilerConfig = {
+  target: '18'
+};
 
 // https://vite.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.gif'],
-  plugins: [react(),
-    babelPlugin({
-      babelConfig:{
-        plugins: ['babel-plugin-react-compiler']
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig]
+        ]
       }
     }),
     tsconfigPath()],
