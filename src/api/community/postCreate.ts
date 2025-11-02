c018a354ef9477d613bd6b0c63c93c86e23b23e3
harinsd404 <harinsd168@gmail.com>
2025-11-02 20:57:51 +0900

    feat::게시글 작성 api 작성

---
+++ src/api/community/postCreate.ts
@@ -0,0 +1,50 @@
+import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { community } from '@/config';
+import api from '../axiosInstance';
+
+interface postCreateInterface {
+  user_id: string;
+  character_id?: number;
+  title: string;
+  body: string;
+  status: string;
+  isBlind?: boolean;
+}
+
+const postCreate = async ({
+  user_id,
+  character_id,
+  title,
+  body,
+  status,
+  isBlind,
+}: postCreateInterface) => {
+  const data = JSON.stringify({
+    user_id: user_id,
+    character_id: character_id,
+    title: title,
+    body: body,
+    status: status,
+    isBlind: isBlind,
+  });
+  try {
+    const response: AxiosResponse = await api.post(`${community}/posts`, data);
+    return true;
+  } catch (error: any) {
+    if (error.response?.data) {
+      console.log(`게시글 작성 실패: ${JSON.stringify(error.response.data)}`);
+    }
+    throw Error;
+  }
+};
+
+export const usePostCreate = () => {
+  return useMutation({
+    mutationFn: postCreate,
+    onSuccess: () => {
+      console.log('게시글 작성 완료');
+    },
+    onError: () => {},
+  });
+};
