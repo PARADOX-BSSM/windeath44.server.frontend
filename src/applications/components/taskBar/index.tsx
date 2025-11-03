c15846da9afea2b837476eba37cfddf8a4cccbe4
bitbyte08 <me@bitworkspace.kr>
2025-11-04 00:06:43 +0900

    feat :: 테스트용 임시 가상 데스크톱 전환 버튼 추가

--- src/applications/components/taskBar/index.tsx
+++ src/applications/components/taskBar/index.tsx
@@ -6,20 +6,49 @@ import { focusAtom, startOptionAtom } from '@/atoms/windowManager';
 import FileImg from '@/assets/search/folder.svg';
 import StartImg from '@/assets/Start.svg';
 import { CURSOR_IMAGES, setCursorImage } from '@/lib/setCursorImg';
+import { useVirtualDesktopManager } from '@/hooks/virtualDesktopManager';
 
 interface TaskBarProps {
   backUpFocus: string;
   setBackUpFocus: React.Dispatch<React.SetStateAction<string>>;
 }
 
+// Virtual Desktop Switcher
+const VirtualDesktopSwitcher = () => {
+  const {
+    virtualDesktopList,
+    virtualCurrentDesktop,
+    switchVirtualDesktop,
+    addVirtualDesktop,
+  } = useVirtualDesktopManager(); // 훅 이름 수정
+
+  return (
+    <div className="flex gap-2 p-2 bg-gray-800 text-white">
+      {virtualDesktopList.map((id) => (
+        <button
+          key={id}
+          className={`px-3 py-1 rounded ${
+            id === virtualCurrentDesktop ? 'bg-blue-600' : 'bg-gray-600'
+          }`}
+          onClick={() => switchVirtualDesktop(id)}
+        >
+          Desktop {id}
+        </button>
+      ))}
+      <button onClick={addVirtualDesktop}>＋</button>
+    </div>
+  );
+};
+
 const TaskBar = ({ backUpFocus, setBackUpFocus }: TaskBarProps) => {
-  const [taskList, ,] = useProcessManager();
+  const [taskList] = useProcessManager();
   const [focus, setFocus] = useAtom(focusAtom);
   const [startOption, setStartOption] = useAtom(startOptionAtom);
 
   return (
     <_.TTaskBar id="taskbarContainer">
       <_.TaskList>
+        <VirtualDesktopSwitcher />
         {taskList.map((task) => {
           if (task.type === 'Shell') {
             return (
@@ -35,7 +64,6 @@ const TaskBar = ({ backUpFocus, setBackUpFocus }: TaskBarProps) => {
                       setBackUpFocus(focus);
                       setFocus('Observer');
                     }
-                    // console.log(startOption);
                     return !prev;
                   });
                 }}
@@ -45,53 +73,36 @@ const TaskBar = ({ backUpFocus, setBackUpFocus }: TaskBarProps) => {
                 <_.StartImg
                   src={StartImg}
                   draggable="false"
-                ></_.StartImg>
+                />
               </_.Observer>
             );
           } else {
-            if ((task.instanceId || task.name) === focus) {
-              return (
-                <_.TaskItem
-                  style={_.taskSelectButtonStyle}
-                  key={task.instanceId || task.name}
-                  onClick={() => {
-                    // setTabDownInterrupt(task.instanceId || task.name);
-                  }}
-                  onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
-                  onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
-                >
-                  <_.ImgContainer
-                    src={task.appSetup?.Image}
-                    draggable="false"
-                  />
-                  <_.TaskName>{task.name}</_.TaskName>
-                </_.TaskItem>
-              );
-            } else {
-              return (
-                <_.TaskItem
-                  style={_.taskButtonStyle}
-                  key={task.instanceId || task.name}
-                  onClick={(e) => {
+            const isFocused = (task.instanceId || task.name) === focus;
+            return (
+              <_.TaskItem
+                style={isFocused ? _.taskSelectButtonStyle : _.taskButtonStyle}
+                key={task.instanceId || task.name}
+                onClick={(e) => {
+                  if(isFocused){
                     e.stopPropagation();
                     setFocus(task.instanceId || task.name);
-                    // console.log(focus);
-                  }}
-                  onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
-                  onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
-                >
-                  <_.ImgContainer
-                    src={task.appSetup?.Image}
-                    draggable="false"
-                  />
-                  <_.TaskName>{task.name}</_.TaskName>
-                </_.TaskItem>
-              );
-            }
+                  }
+                }}
+                onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
+                onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
+              >
+                <_.ImgContainer
+                  src={task.appSetup?.Image}
+                  draggable="false"
+                />
+                <_.TaskName>{task.name}</_.TaskName>
+              </_.TaskItem>
+            );
           }
         })}
       </_.TaskList>
     </_.TTaskBar>
   );
 };
+
 export default TaskBar;
