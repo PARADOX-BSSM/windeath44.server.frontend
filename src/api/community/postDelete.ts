57db187a8a98439c4d61b6985355007494972503
harinsd404 <harinsd168@gmail.com>
2025-11-04 01:16:19 +0900

    feat:: postDelete api 작성

---
+++ src/api/community/postDelete.ts
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { community } from '@/config';
+import api from '../axiosInstance';
+
+const postDelete = async (post_id: number) => {
+  try {
+    const response: AxiosResponse = await api.delete(`${community}/posts/${post_id}`);
+  } catch (error: any) {
+    if (error.response?.data) {
+      console.log('게시글 삭제 실패', Error);
+    }
+    throw Error;
+  }
+};
+
+export const usePostDelete = () => {
+  return useMutation({
+    mutationFn: postDelete,
+    onSuccess: () => {},
+    onError: () => {},
+  });
+};
