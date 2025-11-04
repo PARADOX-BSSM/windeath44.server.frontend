93778101c0f23ca87b0a2fa49ef54fa888f03759
roena0516 <tkf2001.yc@gmail.com>
2025-11-05 00:04:42 +0900

    feat :: 설정 동적 생성

--- src/applications/applicationList/settings/index.tsx
+++ src/applications/applicationList/settings/index.tsx
@@ -1,6 +1,6 @@
 import * as _ from './style';
 import { useAtom } from 'jotai';
-import { settingsAtom, type ScreenRatio } from '@/atoms/settings';
+import { settingsAtom, settingsConfig, type SettingsState } from '@/atoms/settings';
 import { setCursorImage, CURSOR_IMAGES } from '@/lib/setCursorImg';
 
 const radioButtonSelected = 'http://localhost:3845/assets/95657640fe36fee6c1e4de89f9edf3543959ea37.svg';
@@ -9,108 +9,74 @@ const radioButtonUnselected = 'http://localhost:3845/assets/e2a6764f10f473ad3025
 const Settings = () => {
   const [settings, setSettings] = useAtom(settingsAtom);
 
-  const handleScreenRatioChange = (ratio: ScreenRatio) => {
-    setSettings({ ...settings, screenRatio: ratio });
+  const handleValueChange = <K extends keyof SettingsState>(key: K, value: SettingsState[K]) => {
+    setSettings({ ...settings, [key]: value });
   };
 
-  const handleCheckboxChange = (field: 'showBootNotification' | 'testCheckbox') => {
-    setSettings({ ...settings, [field]: !settings[field] });
+  const handleCheckboxToggle = (key: keyof SettingsState) => {
+    setSettings({ ...settings, [key]: !settings[key] });
   };
 
   return (
     <_.Container>
       <_.WindowFrame>
-        <_.Setting>
-          <_.SectionTitle>화면 비율</_.SectionTitle>
-          <_.InputContainer>
-            <_.Input>
-              <_.Label
-                onClick={() => handleScreenRatioChange('4:3')}
-                onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
-                onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
-              >
-                <_.RadioButtonIcon>
-                  <img
-                    src={settings.screenRatio === '4:3' ? radioButtonSelected : radioButtonUnselected}
-                    alt="4:3 radio"
-                  />
-                </_.RadioButtonIcon>
-                <_.LabelText>4:3</_.LabelText>
-                <_.HiddenInput
-                  type="radio"
-                  name="screenRatio"
-                  value="4:3"
-                  checked={settings.screenRatio === '4:3'}
-                  onChange={() => handleScreenRatioChange('4:3')}
-                />
-              </_.Label>
-            </_.Input>
-            <_.Input>
-              <_.Label
-                onClick={() => handleScreenRatioChange('16:9')}
-                onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
-                onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
-              >
-                <_.RadioButtonIcon>
-                  <img
-                    src={settings.screenRatio === '16:9' ? radioButtonSelected : radioButtonUnselected}
-                    alt="16:9 radio"
-                  />
-                </_.RadioButtonIcon>
-                <_.LabelText>16:9</_.LabelText>
-                <_.HiddenInput
-                  type="radio"
-                  name="screenRatio"
-                  value="16:9"
-                  checked={settings.screenRatio === '16:9'}
-                  onChange={() => handleScreenRatioChange('16:9')}
-                />
-              </_.Label>
-            </_.Input>
-          </_.InputContainer>
-        </_.Setting>
+        {settingsConfig.map((section, sectionIndex) => (
+          <_.Setting key={sectionIndex}>
+            <_.SectionTitle>{section.title}</_.SectionTitle>
+            <_.InputContainer>
+              {section.items.map((item, itemIndex) => {
+                if (item.type === 'radio' && item.options) {
+                  return item.options.map((option) => (
+                    <_.Input key={`${itemIndex}-${String(option.value)}`}>
+                      <_.Label
+                        onClick={() => handleValueChange(item.key, option.value)}
+                        onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
+                        onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
+                      >
+                        <_.RadioButtonIcon>
+                          <img
+                            src={settings[item.key] === option.value ? radioButtonSelected : radioButtonUnselected}
+                            alt={`${option.label} radio`}
+                          />
+                        </_.RadioButtonIcon>
+                        <_.LabelText>{option.label}</_.LabelText>
+                        <_.HiddenInput
+                          type="radio"
+                          name={item.key}
+                          value={String(option.value)}
+                          checked={settings[item.key] === option.value}
+                          onChange={() => handleValueChange(item.key, option.value)}
+                        />
+                      </_.Label>
+                    </_.Input>
+                  ));
+                }
 
-        <_.Setting>
-          <_.SectionTitle>알림</_.SectionTitle>
-          <_.InputContainer>
-            <_.Input>
-              <_.Label
-                onClick={() => handleCheckboxChange('showBootNotification')}
-                onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
-                onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
-              >
-                <_.CheckboxIcon checked={settings.showBootNotification} />
-                <_.LabelText>부팅 시 알림 표시</_.LabelText>
-                <_.HiddenInput
-                  type="checkbox"
-                  checked={settings.showBootNotification}
-                  onChange={() => handleCheckboxChange('showBootNotification')}
-                />
-              </_.Label>
-            </_.Input>
-          </_.InputContainer>
-        </_.Setting>
+                if (item.type === 'checkbox') {
+                  return (
+                    <_.Input key={itemIndex}>
+                      <_.Label
+                        onClick={() => handleCheckboxToggle(item.key)}
+                        onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
+                        onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
+                      >
+                        <_.CheckboxIcon checked={Boolean(settings[item.key])} />
+                        <_.LabelText>{item.label}</_.LabelText>
+                        <_.HiddenInput
+                          type="checkbox"
+                          checked={Boolean(settings[item.key])}
+                          onChange={() => handleCheckboxToggle(item.key)}
+                        />
+                      </_.Label>
+                    </_.Input>
+                  );
+                }
 
-        <_.Setting>
-          <_.SectionTitle>테스트 설정</_.SectionTitle>
-          <_.InputContainer>
-            <_.Input>
-              <_.Label
-                onClick={() => handleCheckboxChange('testCheckbox')}
-                onMouseEnter={() => setCursorImage(CURSOR_IMAGES.hand)}
-                onMouseLeave={() => setCursorImage(CURSOR_IMAGES.default)}
-              >
-                <_.CheckboxIcon checked={settings.testCheckbox} />
-                <_.LabelText>선택되지 않은 체크박스 예시</_.LabelText>
-                <_.HiddenInput
-                  type="checkbox"
-                  checked={settings.testCheckbox}
-                  onChange={() => handleCheckboxChange('testCheckbox')}
-                />
-              </_.Label>
-            </_.Input>
-          </_.InputContainer>
-        </_.Setting>
+                return null;
+              })}
+            </_.InputContainer>
+          </_.Setting>
+        ))}
       </_.WindowFrame>
     </_.Container>
   );
--- src/atoms/settings.ts
+++ src/atoms/settings.ts
@@ -5,7 +5,6 @@ export type ScreenRatio = '4:3' | '16:9';
 export interface SettingsState {
   screenRatio: ScreenRatio;
   showBootNotification: boolean;
-  testCheckbox: boolean;
   // 추후 언어, 표시 설정 등 추가 가능
   // language: 'ko' | 'en';
   // displaySettings: {
@@ -17,5 +16,52 @@ export interface SettingsState {
 export const settingsAtom = atomWithStorage<SettingsState>('settings', {
   screenRatio: '4:3',
   showBootNotification: true,
-  testCheckbox: false,
 });
+
+// 설정 스키마 타입 정의
+export type SettingInputType = 'radio' | 'checkbox';
+
+export interface RadioOption<T> {
+  value: T;
+  label: string;
+}
+
+export interface SettingItem<K extends keyof SettingsState = keyof SettingsState> {
+  type: SettingInputType;
+  key: K;
+  label: string;
+  options?: RadioOption<SettingsState[K]>[];
+}
+
+export interface SettingSection {
+  title: string;
+  items: SettingItem[];
+}
+
+// 설정 항목 리스트 정의
+export const settingsConfig: SettingSection[] = [
+  {
+    title: '화면 비율',
+    items: [
+      {
+        type: 'radio',
+        key: 'screenRatio',
+        label: '',
+        options: [
+          { value: '4:3', label: '4:3' },
+          { value: '16:9', label: '16:9' },
+        ],
+      },
+    ],
+  },
+  {
+    title: '알림',
+    items: [
+      {
+        type: 'checkbox',
+        key: 'showBootNotification',
+        label: '부팅 시 알림 표시',
+      },
+    ],
+  },
+];
