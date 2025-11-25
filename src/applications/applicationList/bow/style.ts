cf9db977e27e29c7c1aa6abf6880f44c8ca28862
soeun823 <bsoeun866@gmail.com>
2025-11-25 09:34:45 +0900

    refactor :: [PW-265] 버튼 위치 수정

--- src/applications/applicationList/bow/style.ts
+++ src/applications/applicationList/bow/style.ts
@@ -14,11 +14,8 @@ export const nbow = styled.div`
 `;
 export const place = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
+  height: fit-content;
+  position: relative;
 `;
 export const imgs = styled.div`
   width: 100%;
@@ -29,48 +26,33 @@ export const imgs = styled.div`
   box-sizing: border-box;
 `;
 export const character = styled.img`
-  width: 20%;
-  height: 50%;
+  width: 100%;
 `;
 export const table = styled.img`
   margin-top: -32%;
   width: 100%;
 `;
+export const PictureContainer = styled.div`
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  position: relative;
+`;
+export const Ribbon = styled.img`
+  width: 100%;
+  position: absolute;
+  top: 0;
+  z-index: 1;
+`;
 export const bbow = styled.div`
-  margin-top: -16%;
-  border-width: 0 1.5px 1.5px 0;
-  border-style: solid;
-  border-color: var(--primary-black);
+  width: 100%;
+  margin: -17% 0 17% 0;
   box-sizing: border-box;
-  & > div {
-    width: 100%;
-    height: 100%;
-    border-width: 1.5px 0 0 1.5px;
-    border-style: solid;
-    border-color: #ffffff;
-    box-sizing: border-box;
-    & > div {
-      width: 100%;
-      height: 100%;
-      border-width: 0 1.5px 1.5px 0;
-      border-style: solid;
-      border-color: var(--dark-primary-color);
-      box-sizing: border-box;
-      & > div {
-        width: 100%;
-        height: 100%;
-        border-width: 1.5px 0 0 1.5px;
-        border-style: solid;
-        border-color: var(--secondary-color);
-        box-sizing: border-box;
-        & > button {
-          border: none;
-          background-color: var(--light-primary-color);
-          font-family: 'Galmuri11';
-          font-size: 16px;
-          padding: 8px 32px;
-        }
-      }
-    }
-  }
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
